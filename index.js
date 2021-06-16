811693c35398149ff86f2f881f6388e92a245809
Ayse Cimen Basar <aysebasar91@gmail.com>
2021-06-16 17:15:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const farmAnimals = 'cow horse sheep pig chicken';
+const [moo, bessie, neigh, neigh, mii ] = farmAnimals.split(' ');
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
